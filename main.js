556e5a19cd4f25d5978ef2a8f1b8623b72e0bae6
lhourquin <lhourquin@gmail.com>
2022-03-27 09:58:22 +0200

    done main.js to fetch api and add advice in html element

---
+++ main.js
@@ -0,0 +1,12 @@
+const adviceId = document.getElementsByClassName("advice-id")[0];
+const advice = document.getElementsByClassName("advice")[0];
+const buttonIconDice = document.getElementsByClassName("icon-dice-container")[0];
+
+buttonIconDice.addEventListener("click", () => {
+    fetch("https://api.adviceslip.com/advice")
+        .then((response) => response.json())
+        .then((result) => {
+            adviceId.innerHTML = "ADVICE #" + result.slip.id;
+            advice.innerHTML = `"${result.slip.advice}"`;
+        })
+})
